9c2c84b20051817ec606cd4d4f55c85ea51845ef
arpit <ajangid@andrew.cmu.edu>
2020-04-14 16:34:09 -0400

    keep only vins-mono

--- rpg_esim/event_camera_simulator/esim_rendering/src/simple_renderer_base.cpp
+++
@@ -1,5 +0,0 @@
-#include <esim/rendering/simple_renderer_base.hpp>
-
-namespace event_camera_simulator {
-
-} // namespace event_camera_simulator
--- rpg_esim/event_camera_simulator/imp/imp_planar_renderer/src/renderer_base.cpp
+++
@@ -1,5 +0,0 @@
-#include <esim/rendering/renderer_base.hpp>
-
-namespace event_camera_simulator {
-
-} // namespace event_camera_simulator
